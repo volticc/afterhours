c7533952da68cec734abc99186f2edf840310eb7
volticc <tylervanpelt08@gmail.com>
2025-12-08 23:24:56 -0500

    new admin seed route

--- pages/api/seed-admin.ts
+++ pages/api/seed-admin.ts
@@ -1,21 +1,21 @@
-import { AdminUserModel } from "@/components/data/resource/admin_user";
-import { getAdminUserDataClient } from "@/config/data-clients";
+import { getAdminUserDataClient } from "../src/components/data/orm/admin_users";
 
 export default async function handler(req, res) {
   try {
     const client = getAdminUserDataClient();
 
-    const admin: AdminUserModel = {
+    const admin = {
       id: "initial_admin",
       email: "tylervanpelt08@gmail.com",
       password: "Bailey2019!!@@",
-      role: "super-admin",
+      role: "super-admin"
     };
 
     await client.setAdminUserById("initial_admin", admin);
 
-    res.status(200).json({ message: "Super admin created" });
+    res.status(200).json({ message: "Super admin created!" });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: error.message });
   }
 }
