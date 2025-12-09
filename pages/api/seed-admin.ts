39b085a16d61eddb385e1f82a160e60d4e1615e0
volticc <tylervanpelt08@gmail.com>
2025-12-09 15:56:46 -0500

    move seed-admin to correct API folder

--- pages/api/seed-admin.ts
+++ pages/api/seed-admin.ts
@@ -1,21 +1,22 @@
-import { getAdminUserDataClient } from "../src/components/data/orm/admin_users";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { DataStoreClient } from "../../components/data/orm/client";
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const client = getAdminUserDataClient();
+    const client = DataStoreClient.getInstance();
 
     const admin = {
       id: "initial_admin",
       email: "tylervanpelt08@gmail.com",
       password: "Bailey2019!!@@",
-      role: "super-admin"
+      role: "super-admin",
     };
 
     await client.setAdminUserById("initial_admin", admin);
 
     res.status(200).json({ message: "Super admin created!" });
-  } catch (error) {
+  } catch (error: any) {
     console.error(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error?.message || "Unknown error" });
   }
 }
